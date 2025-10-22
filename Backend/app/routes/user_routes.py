8ed198aba8f55226aeb6d95284f28eab4af1d13f
NatalieGichuki <nataliegichuki@gmail.com>
2025-10-22 22:09:37 +0300

    Defining the user routes and all CRUD operations

---
+++ Backend/app/routes/user_routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models.user import User
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+@user_bp.route('/profile', methods=['GET', 'OPTIONS'])
+@jwt_required()
+def get_user_profile():
+    if request.method == 'OPTIONS':
+        return jsonify({"message": "CORS preflight request successful"}), 200
+    
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    return jsonify({"id": user.id, "email": user.email, "name": user.name, "gender": user.gender}), 200
+
+
+@user_bp.route('/profile', methods=['PUT', 'OPTIONS'])
+@jwt_required()
+def update_user_profile():
+    if request.method == 'OPTIONS':
+        return jsonify({"message": "CORS preflight request successful"}), 200
+    
+    user_id = get_jwt_identity()
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.get_json()
+
+    if not data: 
+        return jsonify({"message": "No data provided"}), 400
+
+    user.name = data.get("name", user.name)
+    user.email = data.get("email", user.email)
+    user.gender = data.get("gender", user.gender)
+
+    db.session.commit()
+
+    return jsonify({"message": "Profile updated successfully"}), 200
+
