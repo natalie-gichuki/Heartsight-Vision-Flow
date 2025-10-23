46b15c8627351db12fd4198efd4f8163afb63112
NatalieGichuki <nataliegichuki@gmail.com>
2025-10-23 17:19:52 +0300

    Route definition

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Swal from 'sweetalert2'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+
   
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
