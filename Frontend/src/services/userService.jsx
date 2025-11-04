166d4b0f527522d67d94d374600d254336b8fdbe
NatalieGichuki <nataliegichuki@gmail.com>
2025-11-04 13:51:51 +0300

    error handling

--- Frontend/src/services/userService.jsx
+++ Frontend/src/services/userService.jsx
@@ -38,7 +38,7 @@ const updateUserProfile = async (profileData) => {
         console.log("PROFILE DATA BEING SENT:", profileData);
 
         const response = await axios.put(
-            `http://localhost:5555'/user/profile`,
+            `http://localhost:5555/user/profile`,
             profileData,
             {
                 headers: {
